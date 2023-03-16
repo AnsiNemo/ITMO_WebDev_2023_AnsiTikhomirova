7a334c94c08701c90dd384f8622c89f1179c54a4
Nemo <purplenema@mail.ru>
2023-03-16 10:35:48 +0300

    HomeWork#2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-export default {
-  server:{
-    port:8088
-  }
-}
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+export default defineConfig({
+  server: {
+    https: true,
+    port: 8888,
+    host: 'local.dev',
+  },
+  plugins: [mkcert()],
+});
